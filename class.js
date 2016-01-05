7f5e86d567e32a44a20b5825d85eb6d6b126b895
pockiest@gmail.com <vagrant@localhost.localdomain>
2016-01-05 18:05:58 +0900

    Add hero icon

--- class.js
+++ class.js
@@ -18,6 +18,122 @@ Board = function(size) {
 
   this.grid = grid;
 
+  this.update = function (move) {
+    this.grid[move.row][move.column] = move.binaryside;
+  }
+
+  this.display = function(player, move) {
+
+  $('#row' + move.row + ' #column' + move.column).addClass('chosen').text(player.side).css('color', player.sidecolor).attr('data-side', player.side);
+
+  }
+
+  this.allyCounter = function(move, count, inc, type) {
+
+    var row = move.row,
+        column = move.column,
+        side = move.binaryside;
+
+    if (type === "vertical"){
+
+      if (row + inc >= 0 && row + inc < this.size) {
+
+        if (this.grid[row + inc][column] === side) {
+
+          count += 1;
+          var move = new Move(row + inc, column, side);
+          return this.allyCounter(move, count, inc, type);
+
+        } else {
+          return count;
+        }
+
+      } else {
+        return count;
+      }
+
+    } else if (type === "horizontal") {
+
+      if (column + inc >= 0 && column + inc < this.size) {
+
+        if (this.grid[row][column + inc] === side) {
+
+          count += 1;
+          var move = new Move(row, column + inc, side);
+          return this.allyCounter(move, count, inc, type);
+
+        } else {
+          return count;
+        }
+
+      } else {
+        return count;
+      }
+
+    } else if (type === "rdiagonal") {
+
+      if (row + inc >= 0 && row + inc < this.size && column + inc >= 0 && column + inc < this.size) {
+
+        if (this.grid[row + inc][column + inc] === side) {
+
+          count += 1;
+          var move = new Move(row + inc, column + inc, side);
+          return this.allyCounter(move, count, inc, type);
+
+        } else {
+          return count;
+        }
+
+      } else {
+        return count;
+      }
+
+    } else {
+
+      if (row - inc >= 0 && row - inc < this.size && column + inc >= 0 && column + inc < this.size) {
+
+        if (this.grid[row - inc][column + inc] === side) {
+
+          count += 1;
+          var move = new Move(row - inc, column + inc, side);
+          return this.allyCounter(move, count, inc, type);
+
+
+        } else {
+          return count;
+        }
+
+      } else {
+        return count;
+      }
+    }
+  }
+
+
+  this.gameOn = function(move) {
+
+    var goalnum = 4;
+
+    if (this.allyCounter(move, 1, 1, "horizontal") + this.allyCounter(move, 1, -1, "horizontal")  >= goalnum + 1) {
+      return false;
+    }
+
+    if (this.allyCounter(move, 1, 1, "vertical") + this.allyCounter(move, 1, -1, "vertical") >= goalnum + 1) {
+      return false;
+    }
+
+    if (this.allyCounter(move, 1, 1, "rdiagonal") + this.allyCounter(move, 1, -1, "rdiagonal") >= goalnum + 1) {
+      return false;
+    }
+
+   if (this.allyCounter(move, 1, 1, "ldiagonal") + this.allyCounter(move, 1, -1, "ldiagonal") >= goalnum + 1) {
+      return false;
+    }
+
+    return true;
+  }
+
+
 }
 
  Move = function(row, column, binaryside) {
@@ -28,117 +144,4 @@ Board = function(size) {
 
 }
 
-Board.prototype.update = function (move) {
-  this.grid[move.row][move.column] = move.binaryside;
-}
 
-Board.prototype.display = function(player, move) {
-
-  $('#row' + move.row + ' #column' + move.column).addClass('chosen').text(player.side).css('color', player.sidecolor).attr('data-side', player.side);
-
-}
-
-Board.prototype.allyCounter = function(move, count, inc, type) {
-
-  var row = move.row,
-      column = move.column,
-      side = move.binaryside;
-
-  if (type === "vertical"){
-
-    if (row + inc >= 0 && row + inc < this.size) {
-
-      if (this.grid[row + inc][column] === side) {
-
-        count += 1;
-        var move = new Move(row + inc, column, side);
-        return this.allyCounter(move, count, inc, type);
-
-      } else {
-        return count;
-      }
-
-    } else {
-      return count;
-    }
-
-  } else if (type === "horizontal") {
-
-    if (column + inc >= 0 && column + inc < this.size) {
-
-      if (this.grid[row][column + inc] === side) {
-
-        count += 1;
-        var move = new Move(row, column + inc, side);
-        return this.allyCounter(move, count, inc, type);
-
-      } else {
-        return count;
-      }
-
-    } else {
-      return count;
-    }
-
-  } else if (type === "rdiagonal") {
-
-    if (row + inc >= 0 && row + inc < this.size && column + inc >= 0 && column + inc < this.size) {
-
-      if (this.grid[row + inc][column + inc] === side) {
-
-        count += 1;
-        var move = new Move(row + inc, column + inc, side);
-        return this.allyCounter(move, count, inc, type);
-
-      } else {
-        return count;
-      }
-
-    } else {
-      return count;
-    }
-
-  } else {
-
-    if (row - inc >= 0 && row - inc < this.size && column + inc >= 0 && column + inc < this.size) {
-
-      if (this.grid[row - inc][column + inc] === side) {
-
-        count += 1;
-        var move = new Move(row - inc, column + inc, side);
-        return this.allyCounter(move, count, inc, type);
-
-
-      } else {
-        return count;
-      }
-
-    } else {
-      return count;
-    }
-  }
-}
-
-
-Board.prototype.gameOn = function(move) {
-
-  var goalnum = 4;
-
-  if (this.allyCounter(move, 1, 1, "horizontal") + this.allyCounter(move, 1, -1, "horizontal")  >= goalnum + 1) {
-    return false;
-  }
-
-  if (this.allyCounter(move, 1, 1, "vertical") + this.allyCounter(move, 1, -1, "vertical") >= goalnum + 1) {
-    return false;
-  }
-
-  if (this.allyCounter(move, 1, 1, "rdiagonal") + this.allyCounter(move, 1, -1, "rdiagonal") >= goalnum + 1) {
-    return false;
-  }
-
- if (this.allyCounter(move, 1, 1, "ldiagonal") + this.allyCounter(move, 1, -1, "ldiagonal") >= goalnum + 1) {
-    return false;
-  }
-
-  return true;
-}
